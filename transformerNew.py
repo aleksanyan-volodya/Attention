a3bc1b494385f254360384227547dbd2fa797351
aleksanyan-volodya <volodya.aleksanyan13@gmail.com>
2026-03-02 20:23:50 +0100

    position encoding done

--- transformerNew.py
+++ transformerNew.py
@@ -66,20 +66,22 @@ class PositionWiseFeedForward(nn.Module):
 
 
 
-class PositionalEncoding(nn.Module):#les encode de position dans la sequence (les transforme en tableau de sinus et cosinus)
+class PositionalEncoding(nn.Module):
+    """Positional encoding to inject sequence order information"""
+
     def __init__(self, d_model: int, max_seq_length: int):
-        super(PositionalEncoding, self).__init__()
+        super().__init__()
         
         pe = torch.zeros(max_seq_length, d_model)
         position = torch.arange(0, max_seq_length, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
-        
-        pe[:, 0::2] = torch.sin(position * div_term)#la on les transforme
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        
         self.register_buffer('pe', pe.unsqueeze(0))
         
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return x + self.pe[:, :x.size(1)]
 
 
