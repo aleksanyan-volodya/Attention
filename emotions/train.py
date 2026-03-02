c0264057986071b69fa8b1922492b12e08136eea
aleksanyan-volodya <volodya.aleksanyan13@gmail.com>
2026-03-02 23:32:22 +0100

    training changed to make it better

--- emotions/train.py
+++ emotions/train.py
@@ -1,88 +1,17 @@
-# This file is the row version of the model 
-# trained IMDB dataset for neg/pos recognition
+"""Training, evaluation, and inference utilities."""
 
-import re	# maybe change if other tokenization algo is used
-from collections import Counter
+from typing import Tuple, List
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-
+from torch.utils.data import DataLoader
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
 
-import sys
+from data import Vocabulary, process_text
+
+import sys 
 sys.path.append("..")
-from transformerNew import Transformer
+from ..transformerNew import Transformer
 
-# using HF datasets
-from datasets import load_dataset
-
-#HYPERPARAM
-VOCAB_SIZE = 20000
-MAX_SEQ_LENGTH = 128
-
-
-def load_split_data(seed=42, dataset_name="imdb"):
-	dataset = load_dataset(dataset_name)
-
-	train = dataset["train"].shuffle(seed)
-	test = dataset["test"].shuffle(seed)
-
-	print("Dataset succefully loaded")
-	print(f"Train samples: {len(train)}")
-	print(f"Test samples: {len(test)}")
-
-	return train, test
-
-def tokenizer(text, train):
-	PAD_IDX = 0
-	UNK_IDX = 1
-
-	# using regex tokenizer: words + contractions + numbers
-	def __regex_tokenizer():
-		text = text.lower()
-		return re.findall(r"[a-z]+(?:'[a-z]+)?|\d+", text)
-
-	print("Building vocabulary")
-	word_counter = Counter()
-
-	for i, sample in enumerate(train):
-		if i>= 10000: 	# use subset to build vocab more quickly
-			break
-		tokens = __regex_tokenizer(sample["text"])
-		word_counter.update(tokens)
-
-	most_commun = word_counter.most_commun(VOCAB_SIZE-2)
-	vocab_dict = {"<pad>": PAD_IDX,
-					"unk>": UNK_IDX}
-	for word, _ in most_commun:
-		vocab_dict[word] = len(vocab_dict)
-
-	MODEL_VOCAB_SIZE = len(vocab_dict)
-	print(MODEL_VOCAB_SIZE)
-
-	
-	# convert text to tensor of tokens ids
-	tokens = __regex_tokenizer(text)
-	ids = [vocab_dict.get(token, UNK_IDX) for token in tokens]
-
-	if len(ids) < MAX_SEQ_LENGTH:
-		ids = ids + [PAD_IDX] * (MAX_SEQ_LENGTH - len(ids))
-	else:
-		ids = ids[:MAX_SEQ_LENGTH]
-
-	return torch.tensor(ids, dtype=torch.long)
-
-
-	
-
-def main():
-	train, test = load_split_data()
-
-
-
-if __name__ == '__main__':
-	print(f"GPU available: {torch.cuda.is_available()}")
-	main()
-	print("end :)")
