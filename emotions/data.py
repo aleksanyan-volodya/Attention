dd2a066a192f6200d6ca93f5ca54729115dc0f57
aleksanyan-volodya <volodya.aleksanyan13@gmail.com>
2026-03-02 22:39:13 +0100

    class just to reuse in other files much simplier and maybe add other tokenizer algos

--- emotions/data.py
+++ emotions/data.py
@@ -2,8 +2,17 @@
 
 import re
 from collections import Counter
-# from typing import ...
+from typing import List, Tuple
 
 import torch
 from torch.utils.data import DataLoader
 from datasets import load_dataset
+
+
+class SmartTokenizer:
+    """Regex-based tokenizer."""
+
+    @staticmethod
+    def tokenize(text: str) -> List[str]:
+        text = text.lower()
+        return re.findall(r"[a-z]+(?:'[a-z]+)?|\d+", text)
