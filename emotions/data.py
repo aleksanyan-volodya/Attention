8a2e1e44eeba86d05839441c22209e97c980414b
aleksanyan-volodya <volodya.aleksanyan13@gmail.com>
2026-03-02 22:42:28 +0100

    created vocab class. Will manege much EZer after :)

--- emotions/data.py
+++ emotions/data.py
@@ -16,3 +16,15 @@ class SmartTokenizer:
     def tokenize(text: str) -> List[str]:
         text = text.lower()
         return re.findall(r"[a-z]+(?:'[a-z]+)?|\d+", text)
+
+class Vocabulary:
+    """Vocabulary mapping tokens to indices."""
+
+    def __init__(self, pad_idx: int = 0, unk_idx: int = 1):
+        self.pad_idx = pad_idx
+        self.unk_idx = unk_idx
+
+        self.token_to_idx = {"<pad>": pad_idx, "<unk>": unk_idx}
+        self.idx_to_token = {pad_idx: "<pad>", unk_idx: "<unk>"}
+        
+        self.vocab_size = 2 # Start with pad and unk
